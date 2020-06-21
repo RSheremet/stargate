2e8bc43f52a586070fd1bd50ea387ea9aae6e829
roman@flores.agency <G34ngkiDeb6sVhtsa2A>
2020-06-21 20:17:37 +0300

    The App is done

--- src/App.js
+++ src/App.js
@@ -1,37 +1,50 @@
 import React, {Component} from 'react'
-import {View, Dimensions} from 'react-native'
+import {ScrollView, StyleSheet, View} from 'react-native'
 import {ImageCard, Header} from "./components/uikit/index"
-import {width, height} from "../constants"
 
 
-const url = 'https://gitlab.com/gHashTag/react-native-init-data/-/raw/master/db.json'
+const url = 'https://gitlab.com/gHashTag/react-native-init/raw/master/db.json'
 
 export default class App extends Component {
-  state = {
-    title: 'STAR GATE',
-    data: []
-  }
-
-  componentDidMount = async() => {
-    try {
-      const response = await fetch(url)
-      const data = await response.json()
-      this.setState({data})
-    } catch (e) {
-      throw e
+    state = {
+        title: 'STAR GATE',
     }
 
-  }
+    componentDidMount = async() => {
+        const response = await fetch(url)
+        const data = await response.json()
+        this.setState({data})
 
-  render() {
-    console.log('width, height', width, height)
-    return (
-        <View>
-          <Header
-              title={this.state.title}
-          />
-          <ImageCard />
-        </View>
-    )
-  }
+    }
+
+    render() {
+        console.log('state', this.state)
+        return (
+            <View style={{flex: 1}}>
+                <Header
+                    title={this.state.title}
+                />
+                <ScrollView>
+                    <View style={styles.imageContainer}>
+                        {this.state.data && this.state.data.map( i =>
+                            <ImageCard
+                                key={i.id}
+                                image={i.image}
+                                name={i.name}
+                            />
+                        )}
+                    </View>
+                </ScrollView>
+            </View>
+        )
+    }
 }
+
+const styles = StyleSheet.create({
+    imageContainer: {
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        justifyContent: 'space-around',
+    }
+})
+
--- src/components/uikit/ImageCard.js
+++ src/components/uikit/ImageCard.js
@@ -1,12 +1,42 @@
 import React from 'react'
-import {Text} from 'react-native'
+import {StyleSheet, Text, Image, View, ScrollView} from 'react-native'
+import {width} from "../../../constants"
 
 const ImageCard = (props) => {
+    const { h1, cover, container } = styles
     return (
-        <Text>
-            ImageCard
-        </Text>
+        <View style={container}>
+            <Image
+                style={cover}
+                source={{uri: props.image}}
+            />
+            <Text style={h1}>
+                {props.name}
+            </Text>
+        </View>
     )
 }
 
 export { ImageCard }
+
+const styles = StyleSheet.create({
+    h1: {
+        fontFamily: 'AvenirNext-DemiBold',
+        fontSize: 18,
+        alignItems: 'center',
+        textAlign: 'center',
+        width: width / 2.4
+    },
+    cover: {
+        width: width / 2.4,
+        height: width * 0.63,
+        borderRadius: 5,
+        marginTop: 10
+    },
+    container: {
+        shadowColor: '#000',
+        shadowRadius: 8,
+        shadowOffset: {width: 0, height: 5},
+        shadowOpacity: 0.4,
+    }
+})
